7ca93f1b01de161d807b9e5467267bf80a653c86
max <cholushkin@gmail.com>
2024-05-14 01:08:30 +0300

    add lesson-03, work in progress

---
+++ lesson-03/readme.md
@@ -0,0 +1,17 @@
+# Integrating a third-party library
+
+- create directory third-party
+- put dependency libs there
+  - glad
+  - glfw
+    - glfw for the purpose of this demo has only minimal am,ount of required files. 
+- link glad 
+  - as lib ?
+  - as sources
+    
+  
+
+
+# What next?
+
+[Lesson 4](../lesson-04/readme.md)
---
+++ lesson-03/sources/main.cpp
@@ -0,0 +1,38 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
